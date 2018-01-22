803d6ef2e8b8b028864149e696bebe850da6ef06
dbfarms <matt.hunger@gmail.com>
2018-01-22 23:50:11 +0000

    tried to fix github omniauth after having changed credentials

--- routes/config/initializers/devise.rb
+++ routes/config/initializers/devise.rb
@@ -3,8 +3,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    
+    config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], provider_ignores_state: true
   
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
